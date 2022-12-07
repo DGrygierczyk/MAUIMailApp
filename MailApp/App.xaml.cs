74528756f3cd8bc7d6f201decb89908d0517ef7f
damiangrygierczyk <damiangrygiercz@gmail.com>
2022-12-07 20:08:55 +0100

    initial commit

---
+++ MailApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MailApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ MailApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MailApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
